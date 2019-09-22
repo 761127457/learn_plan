796baf70a47699809ddd7338c63bdb2718667c8c
xiaowen <761127457@qq.com>
2019-09-22 14:16:24 +0800

    2019.9.22

---
+++ Python/Python程序语言设计/scale.py
@@ -0,0 +1,13 @@
+import time
+print("--开始执行")
+#for i in range(101):
+ # print("\r{:3}%".format(i),end="")
+  #time.sleep(0.1)
+	
+import random
+a = random.randint(10,20)
+print(a)
+
+f = lambda x,y : x+y
+
+print(f(2,4))
